98a09c83c9dd33fb8bef53cf441713c104d1740a
Will <wdputnam42@gmail.com>
2023-09-11 15:57:56 -0400

    did I fix?

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose');
 // mongoose.set('strictQuery', false);
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/new-space'
-);
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/new-space');
 
 module.exports = mongoose.connection;
+
+
+// ,
+//     {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true
+//     }
