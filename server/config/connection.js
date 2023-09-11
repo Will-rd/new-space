e5e28a231378f45ca7cf0e67f4cf95be60d01575
Will <wdputnam42@gmail.com>
2023-09-11 15:15:52 -0400

    testing modified config.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,7 @@
 const mongoose = require('mongoose');
-mongoose.set('strictQuery', false);
+// mongoose.set('strictQuery', false);
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/new-space',
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/new-space'
 );
 
 module.exports = mongoose.connection;
