8ec9df1a3446ea63463cead03c0716c6fb9965ed
Will <wdputnam42@gmail.com>
2023-09-11 14:07:24 -0400

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,11 @@
 const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
 
-mongoose.connect(process.env.MONGOD_URI || 'mongodb://127.0.0.1:27017/new-space');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/new-space',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+);
 
 module.exports = mongoose.connection;
